af0c327b5944b3aaa5f56a3b7e2d5f16898eed6b
koenvandermijeschool-dot <koenvandermijeschool@gmail.com>
2025-12-08 13:41:42 +0100

    Create new_otbord.h

---
+++ new_otbord.h
@@ -0,0 +1,56 @@
+class vakje {
+    private:
+        char inhoud; // . of 'W' of 'Z'
+        vakje* buurvakjes[8]; // Alle vakjes die naast het vakje kan liggen
+
+    public:
+        vakje();
+        char geefInhoud();
+        void zetInhoud(char c);
+        vakje* geefBuur(int richting);
+        void zetBuur(int richting, vakje* buurvakje);
+        vakje* maakRij(int breedte);
+        vakje* maakBord(int breedte, int hoogte);
+        void verwijderBord(vakje* linksboven);
+};
+
+class OthelloBord {
+    private:
+        vakje* linksboven;
+        int breedte;
+        int hoogte;
+        char huidigeSpeler;  // 'W' of 'Z'
+        bool witComputer;
+        bool zwartComputer;
+        bool nietafdrukken;  
+        
+    public:
+        OthelloBord(int b, int h);  // Constructor
+        ~OthelloBord();             // Destructor
+        void afdrukken();
+        bool doeZet(char kolom, int rij);
+        char geefHuidigeSpeler();
+        void wisselSpeler();
+        vakje* vindVakje(int rij, int kolom);
+        bool isZetGeldig(vakje* v, char speler);
+        int telOmslaanInRichting(vakje* v, int richting, char speler);
+        void slaOmInRichting(vakje* v, int richting, char speler, int aantal);
+        
+        // Nieuwe functies voor computer speler
+        bool heeftGeldigeZetten(char speler);
+        void doeComputerZet();
+        void zetWitComputer(bool isComputer);
+        void zetZwartComputer(bool isComputer);
+        bool isHuidigeSpelerComputer();
+        bool isSpelAfgelopen();
+        int telStenen(char speler);
+        void toonEindstand();
+        bool getWitComputer();
+        bool getZwartComputer();
+        void speelMeerdereSpellen(int aantalSpellen);
+        void resetBord();
+        void zetNietAfdrukken(bool a);
+
+};
+
+void menu();
